d22d59ea5e66086f804c2494e33e8a994a6fd4d3
Rafael Dias <rafaelldiias100@gmail.com>
2022-10-17 17:25:54 -0300

    Criação do Header.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import LogoSmall from "../../assets/logo-pequeno.png";
+import Logo from "../../assets/logo.png";
+import Person from "../../assets/participante.png";
+
+import { HeaderContainer } from "./styles";
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <img src={LogoSmall} alt="Logo" className="logoSmall" />
+      <img src={Logo} alt="Logo" className="logo" />
+      <img src={Person} alt="Pessoa" className="person" />
+    </HeaderContainer>
+  );
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  position: relative;
+  height: 50vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .logoSmall {
+    width: 18.6rem;
+    height: 15.7rem;
+  }
+  .person {
+    position: absolute;
+    bottom: -2.1rem;
+    width: 31rem;
+  }
+
+  .logo {
+    display: none;
+  }
+
+  @media (min-width: 1024px) {
+    flex-direction: row;
+    margin-left: 9%;
+    justify-content: left;
+
+    .logoSmall {
+      display: none;
+    }
+    .logo {
+      display: block;
+    }
+    .person {
+      position: absolute;
+      bottom: -3.3rem;
+      right: 9%;
+      width: 45rem;
+    }
+  }
+`;
