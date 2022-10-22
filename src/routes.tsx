97cf6b155614669cae1c7cc3ffb45a036dd33fe6
Rafael Dias <rafaelldiias100@gmail.com>
2022-10-22 17:25:55 -0300

    Adicionando proteção na página de sorteio para só poder acessar com a lista de pessoas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import useListPeople from "state/hooks/useListPeople";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Raffle from "./pages/Raffle";
+
+function ProtectedRoutes({ redirectTo }: { redirectTo: string }) {
+  const listPeople = useListPeople();
+  console.log(listPeople);
+  return listPeople.length !== 0 ? <Outlet /> : <Navigate to={redirectTo} />;
+}
 
 export default function AppRouter() {
   return (
@@ -11,7 +24,9 @@ export default function AppRouter() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/sorteio" element={<Raffle />} />
+          <Route element={<ProtectedRoutes redirectTo={"/"} />}>
+            <Route path="/sorteio" element={<Raffle />} />
+          </Route>
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
